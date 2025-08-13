fc1bfb27db1a0a04d59d8f7f147720cc055920c8
Ajith Sasi <itsmeajithsasi5@gmail.com>
2025-08-12 21:43:27 -0700

    Update nfs.sh

--- 06-NFS-Automount-Infrastructure-Setup/nfs.sh
+++ 06-NFS-Automount-Infrastructure-Setup/nfs.sh
@@ -1,56 +1,67 @@
-        ##CLINT CONFIGERATION ###
--------------------------------------------------
-# to change hostname
-nmtui
+##SERVER CONFIG ####
+--------------------
+
+# Change hostname (interactive)
+sudo nmtui
 exec bash
-#to install nfs utils
-yum install -y nfs-utils
-#find ip
- ifconfig
-#start and enable
-systemctl start nfs-server
-systemctl enable nfs-server
-systemctl status nfs-server
-#install auto fs for automatic mounting
-yum install -y autofs
-#start and enable
-systemctl enable autofs
-systemctl start autofs
-systemctl enable autofs
-systemctl status autofs
-#make mount folder 
-mkdir /mnt/mount
-#edit config file
-vim /etc/auto.master.d/new.autofs
-vim /etc/auto.misc
-#after editing config file restart service
-systemctl restart nfs-server
-systemctl restart autofs
-#to access nfs shared dir
+
+# Install NFS server utilities
+sudo yum install -y nfs-utils
+
+# Start and enable NFS server
+sudo systemctl enable --now nfs-server
+sudo systemctl status nfs-server
+
+# Create directory to share
+sudo mkdir -p /myfiles
+sudo touch /myfiles/file /myfiles/file1
+
+# Edit /etc/exports to specify NFS sharing (example)
+# Example line: /myfiles nfs-client-ip(rw,sync,no_root_squash,no_subtree_check)
+
+# Apply export changes
+sudo exportfs -rv
+
+# Restart NFS server to apply changes
+sudo systemctl restart nfs-server
+
+# Check IP address (if ifconfig not available, use ip a)
+ifconfig || ip a
+
+# (Optional) Change ownership if needed
+# sudo chown nobody:nobody /myfiles
+
+
+
+### CLIENT CONFIG ####
+------------------------
+
+# Change hostname (interactive)
+sudo nmtui
+exec bash
+
+# Install necessary packages
+sudo yum install -y nfs-utils autofs
+
+# Check IP address (if ifconfig not available, use ip a)
+ifconfig || ip a
+
+# Start and enable autofs (no need to start nfs-server on client)
+sudo systemctl enable --now autofs
+sudo systemctl status autofs
+
+# Create mount point
+sudo mkdir -p /mnt/mount
+
+# Edit /etc/auto.master.d/new.autofs (example)
+# Example content: /mnt/mount /etc/auto.misc
+
+# Edit /etc/auto.misc to add mount (example)
+# Example line: nfs_share -rw,soft,intr nfs-server-ip:/myfiles
+
+# After editing config files, restart services
+sudo systemctl restart autofs
+
+# To access NFS shared directory
 cd /mnt/mount
-
-
-### SERVER CONFIGERATION  #######
------------------------------------
-#change host name
-nmtui
-exec bash
-#install nfs and start+enable
-yum install -y nfs-utils
-systemctl start nfs-server
-systemctl enable nfs-server
-#for shareing a dir.make one
-mkdir /myfiles
-touch /myfiles/file
-touch /myfiles/file1
-#specify nfs shareing dir location
-vim /etc/exports
-#restart nfs
-systemctl restart nfs-server
-#export details
-exportfs -rv
-#ip address
-ifconfig
-#change ownership to nobody for access from clint
-chown nobody /myfiles
-
+ls
